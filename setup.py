64eab198143d2132b81087e5f76a4c924b7e4d79
hvelarde <hector.velarde@gmail.com>
2012-05-04 20:21:31 -0300

    package status must be Alpha at this time

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='s17.person.portlets',
       description="White pages, birthday and profile portlets for person",
       long_description=long_description,
       classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.1",
