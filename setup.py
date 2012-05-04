c2e63cc2feb3a9439fd72ebb4324bf9e95ee31b7
hvelarde <hector.velarde@gmail.com>
2012-05-04 20:19:50 -0300

    updated package information

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
-from setuptools import setup, find_packages
-import os
+# -*- coding:utf-8 -*-
 
+import os
+from setuptools import setup, find_packages
+
+version = '1.0.dev0'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='s17.person.portlets',
-      version='0.1',
+      version=version,
       description="White pages, birthday and profile portlets for person",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
         "Framework :: Plone :: 4.1",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Framework :: Plone :: 4.2",
+        "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Office/Business",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='portlet person portlets',
@@ -21,18 +34,19 @@ setup(name='s17.person.portlets',
       url='https://github.com/simplesconsultoria/s17.person.portlets',
       license='gpl',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['s17', 's17.person'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
         'setuptools',
         'collective.person',
-      ],
+        ],
       extras_require={
         'test': ['plone.app.testing'],
         },
       entry_points="""
-      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
