45983f23bf556be470a965b2473a2ec5729e2735
Alejandro Pereira <alepereira86@gmail.com>
2012-06-27 19:14:21 -0300

    Adding tests cases for portlets with anonymous users.

---
+++ src/s17/person/portlets/tests/test_doctest.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+
+import unittest2 as unittest
+import doctest
+
+from plone.testing import layered
+
+from s17.person.portlets.testing import FUNCTIONAL_TESTING
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(doctest.DocFileSuite('tests/portlets_with_anonymous.txt',
+                                     package='s17.person.portlets'),
+                layer=FUNCTIONAL_TESTING),
+        ])
+    return suite
